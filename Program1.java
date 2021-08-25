05b5cb4ac4390b33be93aff9c9c35ca0804a155c
KhandadeSuchita <89135361+KhandadeSuchita@users.noreply.github.com>
2021-08-25 13:24:47 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,20 @@
+public class Code1{
+ 
+ public static void main(String[] args) {
+  System.out.print("Result of 1st expression is : ");
+  
+  System.out.println(-1+4*6);
+  
+   System.out.print("Result of 2nd expression is : ");
+   
+  System.out.println((35+5)%7);
+  
+  System.out.print("Result of 3rd expression is : ");
+  
+  System.out.println(14+-4*6/11);
+  
+  System.out.print("Result of 4th expression is : ");
+  
+  System.out.println(2+15/6*1-7%2);
+ }
+ }
